2a35e802ef73923c9d66e20b5a45c974ff8a6532
Ayush Mishra <64149749+ayush4545@users.noreply.github.com>
2026-04-12 12:18:08 +0530

    Refactor tab layout and add SafeAreaView to tabs; update HOME_USER name and add clsx dependency

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import "@/global.css"
 import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import { styled } from "nativewind";
  
+const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
+    <SafeAreaView className="flex-1 bg-background p-5">
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
       
     </View>
+    </SafeAreaView>
   );
 }
