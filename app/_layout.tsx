774db7171dfe1c6884e0c4527435d8f728838290
Ayush Mishra <64149749+ayush4545@users.noreply.github.com>
2026-04-12 13:11:28 +0530

    Add font loading and splash screen handling to AuthLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,30 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "@/global.css";
+import { useEffect } from "react";
+import {useFonts} from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function AuthLayout() {
+  
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
+  })
+
+  useEffect(() => {
+    // Hide splash only when both fonts and auth are loaded
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  // Don't render app until both are ready
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{headerShown: false}}/>;
 }
