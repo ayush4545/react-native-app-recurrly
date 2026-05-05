a3f7b66e11617467bb08d8fa1c652a00954ef498
Ayush Mishra <64149749+ayush4545@users.noreply.github.com>
2026-05-05 18:30:26 +0530

    feat: update app configuration and add EAS build settings

--- app.config.js
+++ app.config.js
@@ -7,6 +7,9 @@ export default {
       ...(appJson.expo?.extra || {}),
       posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
       posthogHost: process.env.POSTHOG_HOST,
+      eas: {
+        projectId: "f5263a61-4766-4e28-a6e3-9d86aad6730e"
+      }
     },
   },
 }
